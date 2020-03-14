3aa80c689ba00e96532955fc64aacbe3eacbbdc9
list17 <1511500409@qq.com>
2020-03-14 13:25:23 +0800

    update arch_install

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,8 @@ module.exports = {
     description: 'Just for fun',
     head: [
         ['link', { rel: 'icon', href: './resource/favicon.png' }]
+    ],
+    plugins: [
+        "vuepress-plugin-live2d-helper"
     ]
 }
